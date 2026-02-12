0c74aa8824b3e0e75b3a1bb1756214b5af74c605
Qiumix <j487428862@gmail.com>
2026-02-12 16:16:50 +0800

    udpate functions.lua

--- lua/functions.lua
+++ lua/functions.lua
@@ -1,10 +1,21 @@
+---
+---Return full url of a github repo
+---
+---@param str string
+---@return string
 function gh(str)
   return 'https://github.com/' .. str
 end
 
-function only_require_without_vsc(plugin)
+---
+---Loads the given module when gui is not vscode,
+---returns any value returned by the given module(`true` when `nil`).
+---
+---@param modname string
+---@return unknown
+function only_require_without_vsc(modname)
   if vim.g.vscode then
-    return
+    return nil
   end
-  return require(plugin)
+  return require(modname)
 end
