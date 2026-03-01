7f5f62589cfd432d89f045ecf31bb4f5b31f5279
Qiumix <j487428862@gmail.com>
2026-03-01 18:37:43 +0800

    mini

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -3,4 +3,8 @@ if not vim.g.vscode then
   require('mini.statusline').setup()
   require('mini.tabline').setup()
   require('mini.icons').setup()
+  require('mini.cmdline').setup()
+  require('mini.cursorword').setup({
+    delay = 0
+  })
 end
