7577aa30c329ba2d1adfe5cd13735e1404443e6d
Qiumix <j487428862@gmail.com>
2026-03-13 10:55:23 +0800

    fix: vscode ftplugin

--- ftplugin/typst.lua
+++ ftplugin/typst.lua
@@ -1,6 +1,8 @@
-require('typst-preview').setup({
-  dependencies_bin = {
-    ['tinymist'] = '/usr/bin/tinymist',
-    ['websocat'] = '/usr/bin/websocat'
-  },
-})
+if not vim.g.vscode then
+  require('typst-preview').setup({
+    dependencies_bin = {
+      ['tinymist'] = '/usr/bin/tinymist',
+      ['websocat'] = '/usr/bin/websocat'
+    },
+  })
+end
