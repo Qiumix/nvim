6f568393c48593ad9898151d92149305a11273bf
Qiumix <j487428862@gmail.com>
2026-04-06 21:15:30 +0800

    ref: add config/misc

--- lua/config/misc/man.lua
+++ lua/config/misc/man.lua
@@ -3,6 +3,9 @@ vim.api.nvim_create_autocmd("FileType", {
   callback = function()
     vim.keymap.set('n', 'd', '<C-d>', { buffer = true, noremap = true })
     vim.keymap.set('n', 'u', '<C-u>', { buffer = true, noremap = true })
+    vim.opt.wrap = true           -- Don't wrap lines
+    vim.opt.number = true         -- Line numbers
+    vim.opt.relativenumber = true -- Relative line numbers
     vim.opt_local.timeoutlen = 0
   end,
 })
