25958d5a28424296fa4c296c7249a9e5b6e9470e
qiumux <j487428862@gmail.com>
2026-04-25 23:28:08 +0800

    feat: add dashboard

---
+++ lua/plugins/ui/dashboard.lua
@@ -0,0 +1,27 @@
+return {
+  "alpha-nvim",
+  after = function()
+    local alpha = require('alpha')
+    local dashboard = require('alpha.themes.dashboard')
+    local start_art = { "gghhh" }
+    dashboard.section.header.val = start_art[5]
+    dashboard.section.buttons.val = {
+      dashboard.button('n', '  New file', ':ene <BAR> startinsert <CR>'),
+      dashboard.button('f', '󰍉  Find file', ':lua require("fzf-lua").files() <CR>'),
+      dashboard.button('e', '  Browse cwd', ':Yazi<CR>'),
+      dashboard.button('r', '  Browse src', ':e ~/.local/src/<CR>'),
+      dashboard.button('s', '󰯂  Browse scripts', ':e ~/scripts/<CR>'),
+      dashboard.button('c', '  Config', ':e ~/.config/nvim/<CR>'),
+      dashboard.button('m', '  Keymaps', ':e ~/.config/nvim/lua/config/keymaps.lua<CR>'),
+      dashboard.button('q', '󰅙  Quit', ':q!<CR>'),
+    }
+
+    dashboard.section.footer.val = function()
+      return vim.g.startup_time_ms or '[[  ]]'
+    end
+
+    dashboard.section.buttons.opts.hl = 'Keyword'
+    dashboard.opts.opts.noautocmd = true
+    alpha.setup(dashboard.opts)
+  end
+}
