f00b5904256f871a43efbc8122281a63bb81df47
Qiumix <j487428862@gmail.com>
2026-04-26 13:22:17 +0800

    ref: rm unneed config for gruvbox

--- lua/plugins/ui/colorscheme.lua
+++ lua/plugins/ui/colorscheme.lua
@@ -1,29 +1,11 @@
+local config = {
+  transparent_mode = true,
+}
+
 return {
   "gruvbox.nvim",
   colorscheme = "gruvbox",
   after = function()
-    require("gruvbox").setup({
-      terminal_colors = true, -- add neovim terminal colors
-      undercurl = true,
-      underline = true,
-      bold = true,
-      italic = {
-        strings = true,
-        emphasis = true,
-        comments = true,
-        operators = false,
-        folds = true,
-      },
-      strikethrough = true,
-      invert_selection = false,
-      invert_signs = false,
-      invert_tabline = false,
-      inverse = true, -- invert background for search, diffs, statuslines and errors
-      contrast = "", -- can be "hard", "soft" or empty string
-      palette_overrides = {},
-      overrides = {},
-      dim_inactive = false,
-      transparent_mode = true,
-    })
+    require("gruvbox").setup(config)
   end,
 }
