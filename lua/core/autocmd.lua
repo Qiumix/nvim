5359c86912023b37b826841469cbed1e82f33c19
Qiumix <j487428862@gmail.com>
2026-04-26 17:01:52 +0800

    feat: autocmd for highlight yank

---
+++ lua/core/autocmd.lua
@@ -0,0 +1,13 @@
+local autocmd = vim.api.nvim_create_autocmd
+local augroup = function(name)
+  return vim.api.nvim_create_augroup("lingshin_" .. name, { clear = true })
+end
+
+-- Highlight on yank
+autocmd("TextYankPost", {
+  desc = "Highlight on yank",
+  group = augroup("highlight_yank"),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
