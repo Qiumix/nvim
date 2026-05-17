b7fe13f84233de7ede9aec1118f8a4c195f35ef8
Qiumix <j487428862@gmail.com>
2026-05-17 11:34:03 +0800

    ref: modularize snack

---
+++ lua/plugins/editor/snack/config.lua
@@ -0,0 +1,23 @@
+return {
+  animate = { enabled = true },
+  bigfile = { enabled = true },
+  dashboard = { enabled = false },
+  dim = { enabled = true },
+  explorer = { enabled = true, replace_netrw = true },
+  image = require("plugins.editor.snack.image"),
+  indent = { enabled = true },
+  input = { enabled = true },
+  layout = { enabled = true },
+  notifier = { enabled = false },
+  quickfile = { enabled = true },
+  scope = { enabled = true },
+  scratch = { enabled = true },
+  scroll = { enabled = true },
+  statuscolumn = { enabled = true },
+  terminal = { enabled = true },
+  toggle = { enabled = true },
+  words = { enabled = false },
+  zen = { enabled = true },
+
+  picker = require("plugins.editor.snack.picker"),
+}
