e01bc265d747ad9660564afc9613ee5e3f172986
Qiumix <j487428862@gmail.com>
2026-05-17 11:39:14 +0800

    ref: modularize flash

--- lua/plugins/editor/flash.lua
+++ lua/plugins/editor/flash.lua
@@ -1,28 +1,4 @@
-local config = {
-  modes = {
-    char = {
-      config = function(opts)
-        opts.autohide = opts.autohide or (vim.fn.mode(true):find("no") and vim.v.operator == "y")
-        opts.jump_labels = opts.jump_labels
-          and vim.v.count == 0
-          and vim.fn.reg_executing() == ""
-          and vim.fn.reg_recording() == ""
-      end,
-      autohide = false,
-      jump_labels = true,
-      multi_line = false,
-      label = { exclude = "hjkliardc" },
-
-      search = { wrap = false },
-      highlight = { backdrop = false },
-      jump = {
-        register = false,
-        autojump = false,
-      },
-    },
-  },
-}
-
+local config = require("plugins.editor.flash.config")
 return {
   "flash.nvim",
   after = function()
