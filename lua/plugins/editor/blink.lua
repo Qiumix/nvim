42d64cef707fbe552f99f2cbd571b920ee921aad
Qiumix <j487428862@gmail.com>
2026-05-17 11:38:31 +0800

    ref: modularize blink

--- lua/plugins/editor/blink.lua
+++ lua/plugins/editor/blink.lua
@@ -1,33 +1,4 @@
-local signature = {
-  enabled = true,
-  window = { border = "rounded" },
-}
-
-local completion = {
-  documentation = { auto_show = true },
-}
-
-local cmdline = {
-  completion = {
-    menu = {
-      auto_show = true,
-    },
-  },
-}
-
-local config = {
-  signature = signature,
-  completion = completion,
-  cmdline = cmdline,
-  appearance = {
-    nerd_font_variant = "mono",
-    use_nvim_cmp_as_default = true,
-  },
-  sources = {
-    default = { "snippets", "lsp", "path", "buffer" },
-  },
-  fuzzy = { implementation = "prefer_rust_with_warning" },
-}
+local config = require("plugins.editor.blink.config")
 
 return {
   "blink.cmp",
