79a61afa62b6900e647b4d92723470b101ece5ef
Qiumix <j487428862@gmail.com>
2026-06-16 12:47:05 +0800

    mini.cmdline

--- lua/plugins/mini/editor.lua
+++ lua/plugins/mini/editor.lua
@@ -6,5 +6,13 @@ return {
     require("mini.cursorword").setup()
     require("mini.surround").setup()
     require("mini.pairs").setup()
+    require("mini.cmdline").setup({
+      autocomplete = { enable = false },
+      autocorrect = { enable = false },
+      autopeek = {
+        enable = true,
+        n_context = 2,
+      },
+    })
   end,
 }
