2dfd33707153f213f25968c2a877f166a677da93
Qiumix <j487428862@gmail.com>
2026-07-15 22:01:40 +0800

    feat: transparent background for blink.cmp

--- lua/core/autocmd.lua
+++ lua/core/autocmd.lua
@@ -11,3 +11,21 @@ autocmd("TextYankPost", {
     vim.hl.hl_op()
   end,
 })
+
+local aug = augroup("BlinkTransparent")
+vim.api.nvim_create_autocmd("User", {
+  group = aug,
+  pattern = "BlinkCmpMenuOpen",
+  once = true,
+  callback = function()
+    vim.api.nvim_set_hl(0, "BlinkCmpMenu", { link = "", bg = "NONE" })
+    vim.api.nvim_set_hl(0, "BlinkCmpMenuBorder", { link = "", bg = "NONE" })
+  end,
+})
+vim.api.nvim_create_autocmd("ColorScheme", {
+  group = aug,
+  callback = function()
+    vim.api.nvim_set_hl(0, "BlinkCmpMenu", { link = "", bg = "NONE" })
+    vim.api.nvim_set_hl(0, "BlinkCmpMenuBorder", { link = "", bg = "NONE" })
+  end,
+})
