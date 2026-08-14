b29bfcbc024832d24a372ea1db2f4345929471ed
Qiumix <j487428862@gmail.com>
2026-08-14 16:20:10 +0800

    mv lsp config to after

---
+++ after/lsp/emmylua_ls.lua
@@ -0,0 +1,30 @@
+local root_markers1 = {
+  ".emmyrc.json",
+  ".emmyrc.lua",
+  ".luarc.json",
+  ".luarc.jsonc",
+}
+local root_markers2 = {
+  ".luacheckrc",
+  ".stylua.toml",
+  "stylua.toml",
+  "selene.toml",
+  "selene.yml",
+}
+
+---@type vim.lsp.Config
+return {
+  cmd = { "emmylua_ls" },
+  filetypes = { "lua" },
+  root_markers = { root_markers1, root_markers2, { ".git" } },
+  workspace_required = false,
+  settings = {
+    emmylua = {
+      codeLens = { enable = true },
+      hint = { enable = true },
+      workspace = {
+        library = vim.api.nvim_get_runtime_file("", true),
+      },
+    },
+  },
+}
